67bce19973b483f501990410c8cd90a99c2e814d
daneker <kkraken2005@gmail.com>
2023-08-18 05:58:35 +0600

    ng module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,29 +1,16 @@
 import { APP_INITIALIZER, NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { KeycloakAngularModule, KeycloakService } from "keycloak-angular";
 import { initializeKeycloak } from "./init/keycloak-init.factory";
 import { HttpClientModule } from "@angular/common/http";
-import { RegistrationComponent } from './components/registration/registration.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatCardModule } from "@angular/material/card";
-import { MatInputModule } from "@angular/material/input";
-import { MatIconModule } from "@angular/material/icon";
-import { ReactiveFormsModule } from "@angular/forms";
-import { MatStepperModule } from "@angular/material/stepper";
-import { MatButtonModule } from "@angular/material/button";
-import { MatOptionModule } from "@angular/material/core";
-import { MatSelectModule } from "@angular/material/select";
-import { MatCheckboxModule } from "@angular/material/checkbox";
-import { MatSidenavModule } from "@angular/material/sidenav";
-import { HeaderComponent } from './components/navigation/header/header.component';
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { FlexLayoutModule } from "@angular/flex-layout";
 import { SidenavComponent } from './components/navigation/sidenav/sidenav.component';
-import { MatListModule } from "@angular/material/list";
-import { MatMenuModule } from "@angular/material/menu";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RegistrationComponent } from './components/registration/registration.component';
+import { HeaderComponent } from './components/navigation/header/header.component';
+import { MaterialModule } from "./material/material.module";
+import { FlexModule } from "./flex/flex.module";
 
 @NgModule({
   declarations: [
@@ -37,21 +24,9 @@ import { MatMenuModule } from "@angular/material/menu";
     AppRoutingModule,
     KeycloakAngularModule,
     HttpClientModule,
-    BrowserAnimationsModule,
-    MatCardModule,
-    MatInputModule,
-    MatIconModule,
     ReactiveFormsModule,
-    MatStepperModule,
-    MatButtonModule,
-    MatOptionModule,
-    MatSelectModule,
-    MatCheckboxModule,
-    MatSidenavModule,
-    MatToolbarModule,
-    FlexLayoutModule,
-    MatListModule,
-    MatMenuModule
+    FlexModule,
+    MaterialModule
   ],
   providers: [
     {
---
+++ frontend/src/app/material/material.module.ts
@@ -0,0 +1,52 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from "@angular/material/card";
+import { MatInputModule } from "@angular/material/input";
+import { MatIconModule } from "@angular/material/icon";
+import { MatStepperModule } from "@angular/material/stepper";
+import { MatButtonModule } from "@angular/material/button";
+import { MatOptionModule } from "@angular/material/core";
+import { MatSelectModule } from "@angular/material/select";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatListModule } from "@angular/material/list";
+import { MatMenuModule } from "@angular/material/menu";
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    MatIconModule,
+    MatStepperModule,
+    MatButtonModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatMenuModule,
+  ],
+  exports:[
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatInputModule,
+    MatIconModule,
+    MatStepperModule,
+    MatButtonModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatCheckboxModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatListModule,
+    MatMenuModule,
+  ]
+})
+export class MaterialModule { }
